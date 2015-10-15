2db62ce905d66cfe09e1eaf3c35ba010558aae17
Samveen <samveen@yahoo.com>
2015-10-15 11:02:02 +0000

    Add session management capability via Beaker middleware

--- scripts/cgiwrapper.py
+++ scripts/cgiwrapper.py
@@ -4,11 +4,23 @@
 import os
 
 from flup.server.fcgi import WSGIServer
+from beaker.middleware import SessionMiddleware
+
 import dispatcher
 
 from conf import config
 
+# Configure the SessionMiddleware
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': False,
+    'session.use_cookie': True,
+    'data_dir': 'data/sessions',
+    'session.data_dir': 'data/sessions'
+}
+
 if __name__ == "__main__":
     oldmask = os.umask (000)
-    WSGIServer(dispatcher.app,bindAddress=config['socket']).run()
+    wsgi_app = SessionMiddleware(dispatcher.app, session_opts)
+    WSGIServer(wsgi_app,bindAddress=config['socket']).run()
     os.umask (oldmask)
