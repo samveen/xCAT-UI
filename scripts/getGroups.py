f78aabc593dfaea2cba058aa85e919d095a80dfe
Samveen <samveen@yahoo.com>
2016-04-01 09:02:28 +0000

    Change groups page to use auth

--- scripts/getGroups.py
+++ scripts/getGroups.py
@@ -1,54 +1,54 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module contains the code for reading data from the sqlite DB and returning it as json
+"""This module returns the groups matching the display group criteria
 """
 
 from conf import config
 
 import subprocess
+import itertools
 
 def main (environ):
-    """ Reads the DB and returns a json result as a list of strings
+    """ Reads the groups and returns a json result as a list of strings
     """
 
     result=[]
 
     # fields: serial,groups,ip,cputype,memory,rack,unit,currstate,status,statustime
-    fd=subprocess.Popen([ "lsdef",
-                          "-t", "node",
-                          "-i", "groups"
-                        ],
-                        shell=False, stdout=subprocess.PIPE).stdout
-    counts = {}
-    for line in fd: 
-        if "=" in line:
-            key,val=line.strip().split('=',2)
-            for i in val.split(","):
-                if 'DisplayGroups' not in config['getGroups'] or i in config['getGroups']['DisplayGroups']:
-                    if i in counts:
-                        counts[i]=counts[i]+1
-                    else:
-                        counts[i]=1
-    result.append('"groups" : [ {0} ]'.format(", ".join(['{{"name":"{k}","count":"{v}"}}'.format(k=k,v=v) for k,v in iter(sorted(counts.iteritems()))])))
-    # result.append(' : [ {0} ]'.format(", ".join(['"{i}"'.format(i=i) for i in groups])))
-    result.append('"msg" : {"status" : "success"}')
+    with subprocess.Popen([ "lsdef", "-t", "group", "-i", "members" ], shell=False, stdout=subprocess.PIPE).stdout as fd:
+        """ OUTPUT: 
+        Object name: kvm
+            members=cacti,cacti1,mta1,netmon1,netproxy1
+        """
+        for gline, mline in itertools.izip(fd,fd):
+            name=gline.strip().split(':',2)[1].strip()
+            if 'DisplayGroups' not in config['getGroups'] or name in config['getGroups']['DisplayGroups']:
+                count=len(mline.strip().split('=',2)[1].strip().split(','))
+                result.append('{{"name":"{n}","count":"{c}"}}'.format(n=name,c=count))
+        result=['"data":{{\n"groups" : [ {0} ]'.format(', '.join(result))]
+        # result.append(' : [ {0} ]'.format(", ".join(['"{i}"'.format(i=i) for i in groups])))
+        result.append('"msg" : {"status" : "success"}\n}')
 
     # Get results and convert to expected json for each row
     ## Expected json form of array of row dicts
     """{
-        "groups" : [
-                     { "name": "G1" : "count" : "c1" }
-                     { "name": "G2" : "count" : "c2" }
-        ],
-        "msg" : { "status" : "success|failure", exception : "Cause" }
+          "data" : {
+            "groups" : [
+                     { "name": "G1" : "count" : "c1" },
+                     { "name": "G2" : "count" : "c2" }, ...
+            ],
+            "msg" : { "status" : "success|failure", exception : "Cause" }
+          }
     }"""
     #for r in c.fetchall():
     #    result.append('{{ {0} }}'.format(", ".join(['"{k}": "{v}"'.format(k=columns[i],v=r[i]) for i in range(len(columns))])))
+    if len(result) == 0:
+        result = ['"error": {"code": 1, "message": "No matching groups found"}']
 
     # make ''.join(result) a valid json array of rows
     result=",\n`".join(result).split('`')
     result.insert(0,"{\n")
-    result.append("}\n")
+    result.append("\n}")
 
     # Cleanup
     # return the results
